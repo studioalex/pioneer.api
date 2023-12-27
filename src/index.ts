1ed7774a921e245a60273df2400f457de5cdbc9b
Alexander Sedeke <alexander.sedeke@gmail.com>
2023-12-27 14:31:04 +0100

    testing elysia

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Elysia } from "elysia";
+import { autoload } from "elysia-autoload";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().use(autoload()).listen(3000);
+console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+export type ElysiaApp = typeof app;
